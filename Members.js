d23f2b459a981d35ca7638e166286689bef86c2c
raycode21 <wanjiku.rachel21@gmail.com>
2023-02-06 09:11:41 +0300

    added data

---
+++ Members.js
@@ -0,0 +1,23 @@
+//Gets all members
+
+const members =  [
+    {
+      id: 1,
+      name: 'John Kamau',
+      email: 'john@gmail.com',
+      status: 'active'
+    },
+    {
+      id: 2,
+      name: 'Mercy Korir',
+      email: 'mercy@gmail.com',
+      status: 'inactive'
+    },
+    {
+      id: 3,
+      name: 'Lucy Nasimiyu',
+      email: 'lucy@gmail.com',
+      status: 'active'
+    }];
+
+module.exports = members;
